2476b19a49a135ab3c3c125400e036adb46fa8cd
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-02-15 10:33:38 +0530

    Starter code

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+function PageIndex() {
+  return (
+    <div>
+      <Head>
+        <title>Hello World</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <p>Hello World</p>
+    </div>
+  );
+}
+
+export default PageIndex;
